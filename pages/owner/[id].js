0e8a225aa171e68a93fb0d795a463f613247f16d
João Pedro Rocha <joao-pedro-vieira-rocha@impulso.network>
2021-08-31 14:31:40 -0300

    change back button name

--- pages/owner/[id].js
+++ pages/owner/[id].js
@@ -83,7 +83,7 @@ export default function New() {
                                 type="button"
                                 className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 onClick={ () => router.push('/owner') }
-                            > Cancelar </button>
+                            > Voltar </button>
                             <button
                                 type="button"
                                 className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
