624e559700c2b9d2cc0afe09bcdfeeaaa94f2b73
João Pedro Rocha <joao-pedro-vieira-rocha@impulso.network>
2021-08-30 21:03:34 -0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa')
+// import withPWA from 'next-pwa'
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+    },
     env: {
         API_URL: process.env.API_URL
     }
-}
+})
